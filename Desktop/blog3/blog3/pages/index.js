2d4d8845506df83ee419320c1cee3a6ffed1ad25
Alex Mackenzie <44316926+AlexMackenzieWS@users.noreply.github.com>
2021-10-17 20:25:17 +0100

    second commit

---
+++ Desktop/blog3/blog3/pages/index.js
@@ -0,0 +1,14 @@
+function homePage() {
+  return (
+    <div>
+      <div>
+        <h1>Welcome!</h1>
+      </div>
+      <div>
+        <p>Test!</p>
+      </div>
+    </div>
+  );
+}
+
+export default homePage;
