fcdbd796602b0e68704e6eb9ab385920450f5925
Alex Mackenzie <44316926+AlexMackenzieWS@users.noreply.github.com>
2022-07-31 18:28:45 +0100

    added socials

--- Desktop/deno_blog/blog/main.ts
+++ Desktop/deno_blog/blog/main.ts
@@ -3,11 +3,14 @@ import blog, { ga, redirects } from "https://deno.land/x/blog@0.4.0/blog.tsx";
 blog({
   title: "Alex Mackenzie",
   description: `
-  Hello. I'm Alex, a developer & partner at Tapestry VC where, quite simply, I try to invest in the world's most important companies. 
-  
-  Below you'll find breakdowns of technical topics, pontifications & the occasional on-job aggregations. Feel free to say hello!`,
+  Hello. I'm Alex, a developer & partner at Tapestry VC where, quite simply, I try to invest in the world's most important companies. Below you'll find breakdowns of technical topics, pontifications & the occasional on-job aggregations.`,
   avatar: "alex.png",
   avatarClass: "rounded-full",
+  links: [
+    { title: "Email", url: "mailto:alex@tapestry.vc" },
+    { title: "GitHub", url: "https://github.com/alexmackenzie-wx" },
+    { title: "Twitter", url: "https://twitter.com/alex__mackenzie" },
+  ],
   author: "Alex Mackenzie",
   background: "#f9f9f9",
 
